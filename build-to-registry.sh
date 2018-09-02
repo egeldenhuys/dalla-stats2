26eec36c453b965af23788a8d52ee4454b4e655b
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-09-02 18:05:44 +0200

    Remove local registry

--- build-to-registry.sh
+++ build-to-registry.sh
@@ -7,3 +7,6 @@ docker push 127.0.0.1:5000/dalla-stats-logger:local
 
 docker build -t 127.0.0.1:5000/dalla-stats-ui:local ./web-server
 docker push 127.0.0.1:5000/dalla-stats-ui:local
+
+docker build -t 127.0.0.1:5000/dalla-stats-db:local ./db
+docker push 127.0.0.1:5000/dalla-stats-db:local
