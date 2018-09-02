4d38c984e6e46f4dc5fabcb5710dfb02cd75f466
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-09-02 16:43:23 +0000

    Change target node and fix latest tag

--- publish.sh
+++ publish.sh
@@ -3,11 +3,17 @@
 # $1 - Version tag
 
 docker build -t egeldenhuys/dalla-stats-logger:$1 ./logger
-docker push egeldenhuys/dalla-stats-logger
+docker tag egeldenhuys/dalla-stats-logger:$1 egeldenhuys/dalla-stats-logger:latest
+docker push egeldenhuys/dalla-stats-logger:$1
+docker push egeldenhuys/dalla-stats-logger:latest
 
-docker build -t egeldenhuys/dalla-stats-ui:$1 ./db
-docker push egeldenhuys/dalla-stats-ui
+docker build -t egeldenhuys/dalla-stats-ui:$1 ./web-server
+docker tag egeldenhuys/dalla-stats-ui:$1 egeldenhuys/dalla-stats-ui:latest
+docker push egeldenhuys/dalla-stats-ui:$1
+docker push egeldenhuys/dalla-stats-ui:latest
 
-docker build -t egeldenhuys/dalla-stats-db:$1 ./web-server
-docker push egeldenhuys/dalla-stats-db
+docker build -t egeldenhuys/dalla-stats-db:$1 ./db
+docker tag egeldenhuys/dalla-stats-db:$1 egeldenhuys/dalla-stats-db:latest
+docker push egeldenhuys/dalla-stats-db:$1
+docker push egeldenhuys/dalla-stats-db:latest
 
